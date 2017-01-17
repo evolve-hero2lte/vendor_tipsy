0a7926585e81a2cd8394ab45d6546bb36daab0c9
deedwar <deed821@gmail.com>
2017-01-17 13:10:09 +1100

    add pixel launcher and wallpaper

--- config/common.mk
+++ config/common.mk
@@ -29,6 +29,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Titania.ogg \
     ro.config.notification_sound=Tethys.ogg
 
+# NexusLauncher
+PRODUCT_COPY_FILES += \
+    vendor/tipsy/prebuilt/NexusLauncher/NexusLauncher.apk:system/app/NexusLauncher/NexusLauncher.apk
+
+# Wallpaper
+PRODUCT_COPY_FILES += \
+    vendor/tipsy/prebuilt/Wallpaper/Wallpaper.apk:system/app/Wallpaper/Wallpaper.apk
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/tipsy/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
